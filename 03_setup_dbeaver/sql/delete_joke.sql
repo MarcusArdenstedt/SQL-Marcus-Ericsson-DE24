b5877d7e492857bbf4b22aa5423c45941dbe2547
Marcus Ericsson <marcus.ericsson_92@icloud.com>
2024-11-15 10:58:24 +0100

    create a tabel, insert data, sorting data and deleted data

---
+++ 03_setup_dbeaver/sql/delete_joke.sql
@@ -0,0 +1,7 @@
+SELECT * FROM main.funny_jokes;
+
+SELECT * FROM main.funny_jokes WHERE rating < 5;
+
+DELETE FROM main.funny_jokes WHERE rating < 5;
+
+SELECT * FROM main.funny_jokes;
---
+++ 03_setup_dbeaver/sql/queries.sql
@@ -0,0 +1,9 @@
+SELECT * FROM main.funny_jokes ORDER BY rating;
+
+SELECT
+	*
+FROM
+	main.funny_jokes
+ORDER BY
+	rating DESC;
+	
