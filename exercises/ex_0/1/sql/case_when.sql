1ac43006017bc63a6aefe0194063ddd3a367fe70
Marcus Ericsson <marcus.ericsson_92@icloud.com>
2024-11-20 15:50:23 +0100

    exercises 1

--- exercises/ex_0/1/sql/case_when.sql
+++ exercises/ex_0/1/sql/case_when.sql
@@ -1,9 +1,12 @@
-SELECT * FROM house_price;
+SELECT * FROM main.house_price;
 
 SELECT
 	(COUNT(CASE WHEN final_price > 10000000 THEN 1 END) * 100 / COUNT(*)) AS percentage_over_10m
 FROM
-	house_price;
+	main.house_price;
+
+SELECT
+	COUNT(*) * 100 / SELECT COUNT(*) main.house_price
+	FROM main.house_price
 
 
-	
