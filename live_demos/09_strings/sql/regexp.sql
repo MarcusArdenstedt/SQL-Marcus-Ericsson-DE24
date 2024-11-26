790735f383c06c689b29b943c78fcaa2341aeb26
Marcus Ericsson <marcus.ericsson_92@icloud.com>
2024-11-26 10:56:08 +0100

    add cleaned strings

---
+++ live_demos/09_strings/sql/regexp.sql
@@ -0,0 +1,14 @@
+-- REGEXP_REPLACE 
+SELECT
+	REGEXP_REPLACE(TRIM(description), ' +', ' ', 'g') AS cleaned_description
+FROM
+	staging.sql_glossary;
+	
+
+
+SELECT
+	*
+FROM
+	staging.sql_glossary
+WHERE
+	LOWER(description) LIKE '%select%';
