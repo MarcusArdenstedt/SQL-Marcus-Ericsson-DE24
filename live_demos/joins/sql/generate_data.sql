81aebaa413da9cf295c348a7a616575ade9ae792
Marcus Ericsson <marcus.ericsson_92@icloud.com>
2024-12-02 22:19:41 +0100

    joins

--- live_demos/joins/sql/generate_data.sql
+++ live_demos/joins/sql/generate_data.sql
@@ -1,54 +1,30 @@
--- creating synthetic data and populating tables 
+-- create plants dataset 
 
-CREATE SCHEMA IF NOT EXISTS synthetic;
+CREATE TABLE IF NOT EXISTS main.plants (
+	plant_id INTEGER PRIMARY KEY,
+	plant_name TEXT UNIQUE,
+	type TEXT
+);
 
-CREATE TABLE IF NOT EXISTS synthetic.sales_jan (
-    id INTEGER,
-    product_name TEXT,
-    amount DECIMAL(10, 2),
-    sale_date DATE
+CREATE TABLE IF NOT EXISTS plant_care (
+    id INTEGER PRIMARY KEY,
+    plant_id INTEGER,
+    water_schedule TEXT,
+    sunlight TEXT
 );
 
 
-CREATE TABLE IF NOT EXISTS synthetic.sales_feb (
-    id INTEGER,
-    product_name TEXT,
-    amount DECIMAL(10, 2),
-    sale_date DATE
-);
+INSERT INTO main.plants (plant_id, plant_name, type) 
+VALUES
+	(1, 'Rose', 'Flower'),
+	(2, 'Oak', 'Tree'),
+	(3, 'Tulip', 'Flower'),
+	(4, 'Cactus', 'Succulent'),
+	(5, 'Sunflower', 'Flower');
 
-
-CREATE TABLE IF NOT EXISTS synthetic.products (
-    id INTEGER,
-    product_name TEXT,
-    category TEXT,
-    price DECIMAL(10, 2)
-);
-
-
-INSERT INTO synthetic.sales_jan VALUES 
-(1, 'Laptop', 1200.50, '2024-01-05'),
-(2, 'Headphones', 200.00, '2024-01-12'),
-(3, 'Monitor', 300.00, '2024-01-15');
-
-
-INSERT INTO synthetic.sales_feb VALUES 
-(3, 'Monitor', 300.00, '2024-02-10'),
-(4, 'Keyboard', 100.00, '2024-02-11'),
-(5, 'Mouse', 50.00, '2024-02-18');
-
-
-INSERT INTO synthetic.products VALUES 
-(1, 'Laptop', 'Electronics', 1200.00), -- duplicate row
-(2, 'Smartphone', 'Electronics', 800.00),
-(3, 'Table', 'Furniture', 150.00),
-(4, 'Chair', 'Furniture', 100.00),
-(5, 'Laptop', 'Electronics', 1200.00); -- duplicate row
-
-
-
-
-
-SELECT * FROM synthetic.sales_jan; 
-
-SELECT * FROM synthetic.sales_feb; 
+INSERT INTO main.plant_care (id, plant_id, water_schedule, sunlight) 
+VALUES
+	(1, 1, 'Daily', 'Full Sun'),
+	(2, 3, 'Weekly', 'Partial Sun'),
+	(3, 4, 'Biweekly', 'Full Sun'),
+	(4, 6, 'Daily', 'Shade');
