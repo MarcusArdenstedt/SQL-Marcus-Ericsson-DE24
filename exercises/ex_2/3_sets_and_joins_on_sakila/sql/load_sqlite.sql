cdf0d9308b41666a0a00628777fe3a2098954fd4
Marcus Ericsson <marcus.ericsson_92@icloud.com>
2024-12-08 13:43:45 +0100

    sets and joins init

---
+++ exercises/ex_2/3_sets_and_joins_on_sakila/sql/load_sqlite.sql
@@ -0,0 +1,4 @@
+INSTALL sqlite;
+LOAD sqlite;
+
+CALL sqlite_attach('/Users/Marcus/Documents/STI/SQL/github/SQL-Marcus-Ericsson-DE24/exercises/ex_2/3_sets_and_joins_on_sakila/data/sqlite-sakila.db');
